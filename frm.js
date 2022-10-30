fc1933c4f0fb6ddad8825366f0317134b939e322
Kristian Gutic <kristian.gutic@gmail.com>
2022-10-30 14:47:46 +0100

    Work in progress

--- frm.js
+++ frm.js
@@ -1,3 +1,109 @@
+class Point {
+  /**
+   * Class for representing point. Either placed by user or auto-generated.
+   * @param {Number} x coordinate within canvas width
+   * @param {Number} y coordinate within canvas height
+   * @param {Number} d diameter
+   * @param {[Number, Number, Number]} color color for of the point
+   * @param {[Number, Number, Number]} selectedColor when point is clicked
+   */
+  constructor(x, y, d = 10, color = [255, 255, 255] , selectedColor = [255, 255, 0]) {
+
+    if (color.length != 3) {
+      throw new Error("Color is of type [Number, Number, Number]");
+    }
+
+    if (!color.every((x) => x >=0 && x <= 255)) {
+      throw new Error("Values for color must be within range <0, 255>");
+    }
+    
+    // x and y coordinates are set wtihin mousclick callback from p5js this
+    // always come from the canvas boundaries
+
+    this.x = x;
+    this.y = y;
+    this.d = d;
+    this.color = color;
+    this.selectedColor = selectedColor;
+    this.isSelected = false;
+  }
+
+  get isSelected() {
+    return this.isSelected;
+  }  
+   
+  click() {
+    this.isSelected = !this.isSelected;
+  }
+
+  draw() {
+    if (this.isSelected)
+      fill(this.selectedColor[0], this.selectedColor[1], this.selectedColor[2]);
+    else
+      fill(this.color[0], this.color[1], this.color[2]);
+    circle(this.x, this.y, this.d);
+  }
+}
+
+class Canvas {
+  /**
+   * Canvas where it all happens.
+   * @param {Number} height 
+   * @param {Number} width 
+   */
+  constructor(height, width) {
+    this.height = height;
+    this.width = width;
+    this.points = []; // Array<Point>
+  }
+
+  /**
+   * Add basic point to the canvas
+   * @param {p5.Vector} coords Vec2, poistion of mouse click
+   */
+  addPoint(coords) {
+    let p = new Point(coords.x, coords.y)
+    this.points.push(p)
+  }
+
+  hasClickedPoint() {
+    // check if point has been clicked
+    // mark it -- so rendering shows this point (selectedColor)
+    // update this point pos based on mouse pos
+    // another point cannot be clicked when one is already clicked
+  }
+
+
+  /**
+   * Method called every frame
+   */
+  update() {
+  }
+}
+
+
+// laod tasks into array ? or activate some task ? diff func ? active task will be displayed
+class Task {
+  constructor() {
+    // callbacks
+    // what vars
+  }
+
+  compute(canvas) {
+    /*
+      Accepts canvas, which contains set of points upon the calculation will be done.
+    */
+    throw new Error("Not implemented!");
+  }
+
+  show() {
+    /*
+      Custom show method for each task.
+      Display the result of the computation.
+    */
+  }
+}
+
 
 let LINES_RES = [];
 
